28bc9e14e1cd69f05e7dfecf8e2ab453b1168b2e
Everton Paiva <evertonpaiva01@gmail.com>
2023-06-17 09:25:35 -0300

    tarefas adicionadas

---
+++ tarefa_1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#----------- Construção de Array Unidimensional e Multidimensional----------- #
+
+#Construção de Array Unidimensional:
+unid_array = np.arange(1,11)
+
+#Construção de Array Multidimensional:
+multd_array = np.array([np.arange(10),np.arange(11,21)])
+
+#print array Unidimensional:
+unid_array
+
+#print array Multidimensional:
+multd_array
+
+
+
+#-----------  Slicing e indexação ----------- #
+
